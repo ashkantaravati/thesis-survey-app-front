b169466eb108f78a8af683b03116e39b2e51c6ca
ashkantaravati <ashkan.taravati@gmail.com>
2021-06-06 19:36:14 +0430

    setup element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img src="./assets/logo.png">
+  <div>
+    <p>
+      If Element Plus is successfully added to this project, you'll see an
+      <code v-text="'<el-button>'"></code>
+      below
+    </p>
+    <el-button type="primary">el-button</el-button>
+  </div>
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
