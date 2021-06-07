90a2b181eb705e4b3d4b6acd844e0deba2ade268
parisaqomi <parisaq97@gmail.com>
2021-06-07 19:12:51 +0430

    add to clipboard

--- src/views/RegisterWizard.vue
+++ src/views/RegisterWizard.vue
@@ -4,46 +4,66 @@
     <el-step title="گام دوم"></el-step>
     <el-step title="گام سوم"></el-step>
     <el-step title="گام چهارم"></el-step>
-  </el-steps> 
+  </el-steps>
   <div class="step-container" v-show="currentStep === 0">
     <h3><strong>گام اول:</strong> مشخصات سازمان و نماینده</h3>
-    <el-input class="pb-1rem"
+    <el-input
+      class="pb-1rem"
       placeholder="نام یا عنوان تجاری سازمان"
       v-model="organizationName"
     ></el-input>
-    <el-input class="pb-1rem"
+    <el-input
+      class="pb-1rem"
       placeholder="نام و نام خانوادگی شما به عنوان نماینده‌ی سازمان"
       v-model="repFullName"
     ></el-input>
-    <el-input class="pb-1rem" placeholder="سمت شما" v-model="repJobTitle"></el-input>
+    <el-input
+      class="pb-1rem"
+      placeholder="سمت شما"
+      v-model="repJobTitle"
+    ></el-input>
     <el-input class="pb-1rem" placeholder="ایمیل" v-model="repEmail"></el-input>
-
   </div>
   <div class="step-container" v-show="currentStep === 1">
     <h3><strong>گام دوم:</strong> اطلاعات تیم‌ها و اعضای تیم‌ها</h3>
     <p>
       نام افراد صرفا برای شناسایی و تفکیک اطلاعات آن هاست. چنانچه تمایل به مخف
-      ماندن نام افراد دارید می‌توانید از نام های مستار اما یکتا استفاده کنید. توجه
-      کنید این نام ها در پرسشنامه برای افراد قابل رویت خواهند بود
+      ماندن نام افراد دارید می‌توانید از نام های مستار اما یکتا استفاده کنید.
+      توجه کنید این نام ها در پرسشنامه برای افراد قابل رویت خواهند بود
     </p>
-    <el-card class="box-card mb-halfrem" v-for="team in teams" :key="team.index">
+    <el-card
+      class="box-card mb-halfrem"
+      v-for="team in teams"
+      :key="team.index"
+    >
       <template #header>
         <div class="card-header">
           <span>اعضای تیم {{ team.index }}</span>
-          <el-button class="button mr-halfrem" type="danger" plain round> <i class="el-icon-delete"></i> حذف تیم</el-button>
+          <el-button class="button mr-halfrem" type="danger" plain round>
+            <i class="el-icon-delete"></i> حذف تیم</el-button
+          >
         </div>
       </template>
-      <el-row v-for="member in team.members" :key="member.index" class="text item d-flex">
-        <el-col :span="2">عضو {{member.index}}</el-col>
+      <el-row
+        v-for="member in team.members"
+        :key="member.index"
+        class="text item d-flex"
+      >
+        <el-col :span="2">عضو {{ member.index }}</el-col>
         <el-col :span="22">
-
-        <el-input class="pb-1rem" v-model="member.name" placeholder="نام عضو" />
+          <el-input
+            class="pb-1rem"
+            v-model="member.name"
+            placeholder="نام عضو"
+          />
         </el-col>
       </el-row>
-      <el-button @click="addTeamMember(team)"  type="primary"> + افزودن عضو </el-button>
+      <el-button @click="addTeamMember(team)" type="primary">
+        + افزودن عضو
+      </el-button>
     </el-card>
 
-    <el-button @click="addTeam" class="p-btn-primary "> + افزودن تیم </el-button>
+    <el-button @click="addTeam" class="p-btn-primary"> + افزودن تیم </el-button>
 
     <!-- <el-button @click="goNext"> بازبینی و ثبت نهایی </el-button> -->
   </div>
@@ -76,17 +96,23 @@
       تیم هایتان تلید شده به تیم ها اقدام فرمایید.
     </p>
     <template v-for="team in teams" :key="team.index">
-      <el-input class="pb-1rem" v-model="team.sharableLink" readonly />
-      <el-button icon="el-icon-document-copy"></el-button>
+      <span class="code text-red">{{ team.sharableLink }}</span>
+      <el-button
+        icon="el-icon-document-copy"
+        @click.stop.prevent="copyTestingCode"
+        >کپی</el-button
+      >
+      <input type="hidden" id="testing-code" :value="team.sharableLink " />
     </template>
   </div>
-  
-
-
 
   <div class="fix-btns-container">
-        <el-button @click="goPrev" :disabled="currentStep !==0 ? disabled : ''">  <i class="el-icon-arrow-right"></i>گام قبل </el-button>
-        <el-button @click="goNext"  :disabled="currentStep !==3 ? disabled : ''"> گام بعد  <i class="el-icon-arrow-left"></i></el-button>
+    <el-button @click="goPrev" :disabled="currentStep !== 0 ? disabled : ''">
+      <i class="el-icon-arrow-right"></i>گام قبل
+    </el-button>
+    <el-button @click="goNext" :disabled="currentStep !== 3 ? disabled : ''">
+      گام بعد <i class="el-icon-arrow-left"></i
+    ></el-button>
   </div>
 </template>
 
@@ -103,6 +129,7 @@ export default defineComponent({
       repEmail: ref(""),
       teams: [],
       lastTeamIndex: 0,
+      testingCode: "1234",
     };
   },
   methods: {
@@ -128,40 +155,53 @@ export default defineComponent({
     addTeamMember(team) {
       team.members.push({ index: 0, name: ref("") });
     },
+    copyTestingCode() {
+      let testingCodeToCopy = document.querySelector("#testing-code");
+      testingCodeToCopy.setAttribute("type", "text");
+      testingCodeToCopy.select();
+      try {
+        alert("با موفقیت کپی شد");
+      } catch (err) {
+        alert("کپی نشد :(");
+      }
+
+      testingCodeToCopy.setAttribute("type", "hidden");
+      window.getSelection().removeAllRanges();
+    },
   },
 });
 </script>
 
 <style>
-.el-steps--horizontal{
-position: relative;
+.el-steps--horizontal {
+  position: relative;
 }
-.el-steps--horizontal:after{
+.el-steps--horizontal:after {
   content: "";
   position: absolute;
   right: 0;
   left: 0;
   width: 100%;
-    height: 2px;
-    background: #d9d9d9;
-    top: 10px;
+  height: 2px;
+  background: #d9d9d9;
+  top: 10px;
 }
-.el-step.is-horizontal .el-step__line{
+.el-step.is-horizontal .el-step__line {
   display: none;
 }
-.step-container{
+.step-container {
   min-height: 300px;
 }
-.fix-btns-container{
+.fix-btns-container {
   background: white;
-    box-shadow: 0 2px 10px rgb(0 0 0 / 20%);
-    border-radius: .5rem;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    position: sticky; 
-    z-index: 1;
-    bottom: 0;
-    margin: 0 auto;
+  box-shadow: 0 2px 10px rgb(0 0 0 / 20%);
+  border-radius: 0.5rem;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
+  position: sticky;
+  z-index: 1;
+  bottom: 0;
+  margin: 0 auto;
 }
 </style>
