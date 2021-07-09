a2f166eb4f94159f2b81dffc4f0c9bd4067a56d7
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-09 12:49:59 +0430

    target production server

--- src/api/gateway.ts
+++ src/api/gateway.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000",
+  // baseURL: "http://localhost:8000",
+  baseURL: "http://thesis.ashkantaravati.ir",
   headers: {
     "Content-type": "application/json",
   },
