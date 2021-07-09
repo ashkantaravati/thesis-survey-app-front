ced4d2a43e114c650dad57f543f8e3d692b0e95b
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-09 19:32:56 +0430

    add a todo

--- src/api/contracts/teamInfoDto.ts
+++ src/api/contracts/teamInfoDto.ts
@@ -2,8 +2,9 @@ import { IDto } from "@/core";
 import { OrganizationSchema, TeamMemberSchema } from "./schema";
 
 export default interface TeamInfoDto extends IDto {
+  // TODO: extend from teamSchema
   id: string;
   name: string;
   members: TeamMemberSchema[];
-  organization: OrganizationSchema;
+  organization: OrganizationSchema; // BUG: too much information
 }
