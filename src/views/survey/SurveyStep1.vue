5332cfa6a0d727aa3333e765e90f51f8e4bae498
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-11 00:04:47 +0430

    implement survey step 1

--- src/views/survey/SurveyStep1.vue
+++ src/views/survey/SurveyStep1.vue
@@ -2,14 +2,14 @@
   <div class="step-container" id="survey-step-1">
     <h3>سوالات عمومی</h3>
     <div class="block mb-halfrem">
-      <p>نام خود را از لیست انتخاب کنید</p>
+      <p>نام</p>
       <el-select
-        v-model="answers.generalQuestions.participantId"
+        v-model="activeParticipant.id"
         filterable
         placeholder="نام خود را از لیست انتخاب کنید"
       >
         <el-option
-          v-for="member in teamInfo.members"
+          v-for="member in teamMembers"
           :key="member.id"
           :label="member.name"
           :value="member.id"
@@ -18,49 +18,61 @@
       </el-select>
     </div>
     <div class="block mb-halfrem">
-      <p>سن خود را از لیست انتخاب کنید</p>
+      <p>سن</p>
       <el-input-number
-        v-model="answers.generalQuestions.participantAge"
+        v-model="generalSurvey.age"
         :min="15"
         :max="80"
       ></el-input-number>
     </div>
     <div class="block mb-halfrem">
-      <p>جنسیت خود را از لیست انتخاب کنید</p>
-      <el-radio-group v-model="answers.generalQuestions.sex" dir="ltr">
+      <p>جنسیت</p>
+      <el-radio-group v-model="generalSurvey.sex" dir="ltr">
         <el-radio-button label="خانم"></el-radio-button>
         <el-radio-button label="آقا"></el-radio-button>
       </el-radio-group>
     </div>
     <div class="block mb-halfrem">
       <p>میزان کل سابقه کار شما ( بر حسب سال)</p>
-      <el-slider
-        v-model="answers.generalQuestions.tenure"
-        show-input
-        step="0.5"
-        :max="40"
-      >
+      <el-slider v-model="generalSurvey.tenure" show-input step="0.5" :max="40">
       </el-slider>
     </div>
     <div class="block mb-halfrem">
       <p>مدت حضور شما در این تیم ( بر حسب ماه)</p>
-      <el-slider
-        v-model="answers.generalQuestions.teamHistory"
-        show-input
-        :max="60"
-      >
+      <el-slider v-model="generalSurvey.teamHistory" show-input :max="60">
       </el-slider>
     </div>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "SurveyStep1",
+  computed: {
+    teamMembers() {
+      return this.$store.state.teamInfo.members;
+    },
+    generalSurvey: {
+      get() {
+        return this.$store.state.survey.generalSurvey;
+      },
+      set(value) {
+        this.$store.commit("setGeneralSurveyResponse", value);
+      },
+    },
+    activeParticipant: {
+      get() {
+        return this.$store.state.activeParticipant;
+      },
+      set(participantId) {
+        console.log(participantId);
+        this.$store.commit("setActiveParticipant", participantId);
+      },
+    },
+  },
 });
 </script>
 
-<style>
-</style>
+<style></style>
