425d3a2cb94defc70195ea9db5daf77183e343af
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-11 23:30:11 +0430

    ctor for VoiceSurvey

--- src/models/VoiceSurvey.ts
+++ src/models/VoiceSurvey.ts
@@ -1,4 +1,6 @@
+import { VOICE_BEHAVIOR_QUESTIONS } from "@/constants";
 import { Model } from "@/core";
+import { LikertResponse } from "./common";
 import { LikertScaleQuestion } from "./common/LikertScaleQuestion";
 import TeamMember from "./TeamMember";
 
@@ -10,4 +12,42 @@ export default class VoiceSurvey extends Model {
   q4!: LikertScaleQuestion;
   q5!: LikertScaleQuestion;
   q6!: LikertScaleQuestion;
+  /**
+   *
+   */
+  constructor(participant: TeamMember) {
+    super();
+    this.id = participant.id;
+    this.evaluatedParticipant = participant;
+    this.q1 = {
+      index: 1,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q1,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+    this.q2 = {
+      index: 2,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q2,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+    this.q3 = {
+      index: 3,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q3,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+    this.q4 = {
+      index: 4,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q4,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+    this.q5 = {
+      index: 5,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q5,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+    this.q6 = {
+      index: 6,
+      text: VOICE_BEHAVIOR_QUESTIONS.Q6,
+      response: new LikertResponse(),
+    } as LikertScaleQuestion;
+  }
 }
