d0f9ac89897edd68f6ec90721e490a875f9ec2a1
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-11 21:02:08 +0430

    remove redundant static property default

--- src/models/common/MinMaxResponse.ts
+++ src/models/common/MinMaxResponse.ts
@@ -3,5 +3,4 @@ import { ISurveyResponse } from "./ISurveyResponse";
 export class MinMaxResponse implements ISurveyResponse {
   min!: number;
   max!: number;
-  static default: MinMaxResponse = { min: 0, max: 0 };
 }
