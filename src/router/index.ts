b1007e2b62afcc89c5009b5948b7876038edf202
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-15 17:05:36 +0430

    scroll to top on step change

--- src/router/index.ts
+++ src/router/index.ts
@@ -106,6 +106,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0 };
+  },
 });
 
 export default router;
