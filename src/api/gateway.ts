3a264c71fca96c8e6be6e035f72505e5f919673e
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-19 00:48:01 +0430

    add a response interceptor (don't know if it works)

--- src/api/gateway.ts
+++ src/api/gateway.ts
@@ -1,8 +1,21 @@
 import axios from "axios";
+import router from "@/router";
+axios.interceptors.response.use(
+  //successful callback, we don't need to do anything
+  (response) => {
+    return response;
+  },
+
+  //check if we received a 404 and redirect
+  (error) => {
+    router.push({ name: "error" });
+    console.error(error);
+  }
+);
 
 export default axios.create({
-  // baseURL: "http://localhost:8000",
-  baseURL: "https://thesis.ashkantaravati.ir",
+  baseURL: "http://localhost:8000",
+  // baseURL: "https://thesis.ashkantaravati.ir",
   headers: {
     "Content-type": "application/json",
   },
