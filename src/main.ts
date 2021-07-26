384a37ce4b8c6f05716fa2631078ca6a425470c8
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-26 23:50:17 +0430

    register ProceedButton as global component

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import App from "./App.vue";
 import installElementPlus from "./plugins/element";
 import router from "./router";
 import store from "./store";
+import ProceedButton from "@/components/common/ProceedButton.vue";
 
 const app = createApp(App);
 installElementPlus(app);
 app.use(VueClipboard);
 app.use(router);
 app.use(store);
+app.component("proceed-button", ProceedButton);
 app.mount("#app");
