bee9e0e08cc06e4577cd5a915bf48c37fe48d3a5
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-27 01:39:11 +0430

    show errors for minmax

--- src/components/survey/MinMaxQuestion.vue
+++ src/components/survey/MinMaxQuestion.vue
@@ -1,33 +1,36 @@
 <template>
-  <div class="mb-halfrem">
-    {{ question.text }}
-  </div>
-  <div>
-    <div class="d-flex align-center" >
-      <span>حداقل:</span>
-      <el-input-number
-        placeholder="کف"
-        v-model="response.min"
-        @change="onMinChange"
-        :min="lowerBound"
-        :max="response.max"
-        :name="question.index + '-min'"
-      >
-      </el-input-number>
+  <el-form-item :error="checkEmpty" :inline-message="true" :required="true">
+    <div class="mb-halfrem">
+      {{ question.text }}
     </div>
-    <div class="mt-1rem d-flex align-center">
-      <span>حداکثر:</span>
-      <el-input-number
-        placeholder="سقف"
-        v-model="response.max"
-        @change="onMaxChange"
-        :min="response.min"
-        :max="upperBound"
-        :name="question.index + '-max'"
-      >
-      </el-input-number>
+    <div>
+      <div class="d-flex align-center">
+        <span>حداقل:</span>
+        <el-input-number
+          :required="true"
+          placeholder="کف"
+          v-model="response.min"
+          @change="onMinChange"
+          :min="lowerBound"
+          :max="response.max"
+          :name="question.index + '-min'"
+        >
+        </el-input-number>
+      </div>
+      <div class="mt-1rem d-flex align-center">
+        <span>حداکثر:</span>
+        <el-input-number
+          placeholder="سقف"
+          v-model="response.max"
+          @change="onMaxChange"
+          :min="response.min"
+          :max="upperBound"
+          :name="question.index + '-max'"
+        >
+        </el-input-number>
+      </div>
     </div>
-  </div>
+  </el-form-item>
 </template>
 
 <script lang="ts">
@@ -40,6 +43,16 @@ export default defineComponent({
     question: { type: MinMaxQuestion, required: true },
   },
   computed: {
+    checkEmpty(): string {
+      if (
+        isNaN(this.question.response.min) ||
+        isNaN(this.question.response.max)
+      )
+        return "حداقل و حداکثر را وارد کنید";
+
+      return "";
+    },
+
     response: {
       get(): MinMaxResponse {
         return this.question.response;
@@ -64,4 +77,8 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style scoped>
+.el-form-item__error {
+  padding-top: 25px !important;
+}
+</style>
--- src/views/survey/SurveyStep2.vue
+++ src/views/survey/SurveyStep2.vue
@@ -8,10 +8,20 @@
       کنید که با حداقل ۹۰٪ اطمینان فکر می‌کنید پاسخ درست در آن بازه قرار دارد.
     </p>
     <el-card class="mb-halfrem">
-      <template v-for="(question, name) in overconfidenceSurvey" :key="name">
-        <min-max-question :question="question" />
-        <el-divider></el-divider>
-      </template>
+      <el-form
+        :model="overconfidenceSurvey"
+        ref="overconfidenceSurveyForm"
+        class="demo-ruleForm"
+      >
+        <template v-for="(question, name) in overconfidenceSurvey" :key="name">
+          <min-max-question
+            ref="question"
+            ruleProp="min"
+            :question="question"
+          />
+          <el-divider></el-divider>
+        </template>
+      </el-form>
     </el-card>
   </div>
   <proceed-button
@@ -28,6 +38,33 @@ import { defineComponent } from "vue";
 export default defineComponent({
   components: { MinMaxQuestion },
   name: "SurveyStep2",
+  methods: {
+    goNext() {
+      const overconfidenceSurveyForm = this.$refs[
+        "overconfidenceSurveyForm"
+      ] as any;
+      overconfidenceSurveyForm.validate((valid: boolean) => {
+        if (valid) {
+          this.$emit("proceed");
+        } else {
+          return false;
+        }
+      });
+    },
+  },
+  data() {
+    return {
+      rules: {
+        min: [
+          {
+            required: true,
+            message: "نمی‌تواند خالی باشد",
+            trigger: "blur",
+          },
+        ],
+      },
+    };
+  },
   computed: {
     overconfidenceSurvey() {
       return this.$store.state.survey.overconfidenceSurvey;
