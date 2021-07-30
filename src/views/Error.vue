3504a9617637bfe024b6e3db751881948c4e1326
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-31 02:23:46 +0430

    fix wrong link

--- src/views/Error.vue
+++ src/views/Error.vue
@@ -1,17 +1,14 @@
 <template>
-    <el-row class="jc-center">
+  <el-row class="jc-center">
     <el-col :xs="24" :lg="18" :xl="18">
       <h4 class="text-center mb-halfrem">
         باعث شرمندگیه... متاسفانه مشکلی پیش آمد.
       </h4>
-     <div class="text-center">
-
-      <router-link :to="{ name: 'register-landing' }">
-        <el-button round type="primary" class="mt-1rem">
-          صفحه اصلی</el-button
-        >
-      </router-link>
-     </div>
+      <div class="text-center">
+        <router-link :to="{ name: 'home' }">
+          <el-button round type="primary" class="mt-1rem"> صفحه اصلی</el-button>
+        </router-link>
+      </div>
       <div class="mt-1rem">
         <img
           class="responsive-img"
