c6942a768878b7a510afbf882d0398fcdcbcc4d3
ashkantaravati <ashkan.taravati@gmail.com>
2021-07-30 21:06:45 +0430

    improve registration success page

--- src/views/registration/RegistrationSuccess.vue
+++ src/views/registration/RegistrationSuccess.vue
@@ -1,23 +1,41 @@
 <template>
   <h3>دریافت لینک برای مشارکت تیم‌ها</h3>
-  <p>
-    از زمانی که برای تکمیل مرحله‌ی اول گذاشتید بسیار سپاسگزارم. حالا می‌توانید
-    لینک‌های ساخته شده برای هر تیم را برای تکمیل پرسشنامه (مرحله‌ی دوم) در
-    اختیار اعضای آن تیم قرار دهید.
-  </p>
-  <template v-for="team in teams" :key="team.index">
-    <el-alert type="info" :closable="false">
-      <router-link
-        style="margin-left:10px"
-        :to="{ name: 'survey-intro', params: { teamId: team.id } }"
-      >
-        {{ team.link }}</router-link
-      >
-      <el-button v-clipboard="team.link" icon="el-icon-document-copy"
-        >کپی</el-button
-      >
-    </el-alert>
-  </template>
+
+  <el-alert
+    type="success"
+    title="ثبت نام با موفقیت انجام شد"
+    center
+    show-icon
+    :closable="false"
+  >
+    <div>
+      <p>
+        از زمانی که برای تکمیل مرحله‌ی اول گذاشتید بسیار سپاسگزارم. حالا
+        می‌توانید لینک‌های ساخته شده برای هر تیم را برای تکمیل پرسشنامه (مرحله‌ی
+        دوم) در اختیار اعضای آن تیم قرار دهید.
+      </p>
+      <ul v-for="team in teams" :key="team.index">
+        <li>
+          <strong>
+            لینک دعوت برای اعضای
+            {{ team.name }}:
+          </strong>
+          <el-link type="info">
+            <router-link
+              style="margin-left:10px"
+              :to="{ name: 'survey-intro', params: { teamId: team.id } }"
+            >
+              {{ team.link }}</router-link
+            >
+          </el-link>
+
+          <el-button v-clipboard="team.link" icon="el-icon-document-copy"
+            >کپی</el-button
+          >
+        </li>
+      </ul>
+    </div>
+  </el-alert>
 </template>
 
 <script>
