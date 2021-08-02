a19b987f4617a9e8bb717102291f31b37e64e2ad
ashkantaravati <ashkan.taravati@gmail.com>
2021-08-02 22:28:13 +0430

    declare $message $alert and $notify

--- src/shims-element-plus.d.ts
+++ src/shims-element-plus.d.ts
@@ -3,5 +3,7 @@ import { ComponentCustomProperties } from "vue";
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
     $alert: any;
+    $message: any;
+    $notify: any;
   }
 }
