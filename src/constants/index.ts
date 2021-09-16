d968f2c7b2a5268044ff8268758d866c98a26f70
ashkantaravati <ashkan.taravati@gmail.com>
2021-09-16 16:35:44 +0430

    set minimum team size to 3 and maximum to 10

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,10 +3,13 @@ import TEAM_COORDINATION_QUESTIONS from "./teamCoordinationQuestions";
 import VOICE_BEHAVIOR_QUESTIONS from "./voiceBehaviorQuestions";
 
 const SITE_TITLE = "پرسش‌نامه‌ی پژوهشی در تیم‌های توسعه‌ی نرم‌افزار";
-
+const MINIMUM_TEAM_SIZE = 3;
+const MAXIMUM_TEAM_SIZE = 10;
 export {
   SITE_TITLE,
   OVERCONFIDENCE_QUESTIONS,
   TEAM_COORDINATION_QUESTIONS,
   VOICE_BEHAVIOR_QUESTIONS,
+  MINIMUM_TEAM_SIZE,
+  MAXIMUM_TEAM_SIZE,
 };
