3eea47d239a0843bdb751133baef70bf58869a04
ashkantaravati <ashkan.taravati@gmail.com>
2021-11-06 00:38:17 +0330

    fix local api address

--- src/api/gateway.ts
+++ src/api/gateway.ts
@@ -1,24 +1,23 @@
 /* eslint-disable no-debugger */
 import axios from "axios";
 import router from "@/router";
-  axios.interceptors.response.use(
-    // successful callback, we don't need to do anything
-    (response) => {
-      return response;
-    },
+axios.interceptors.response.use(
+  // successful callback, we don't need to do anything
+  (response) => {
+    return response;
+  },
 
-    // check if we received a 404 and redirect
-    (error) => {
-      router.push({ name: "error" });
-      console.error(error);
-    }
-  );
+  // check if we received a 404 and redirect
+  (error) => {
+    router.push({ name: "error" });
+    console.error(error);
+  }
+);
 
 export default axios.create({
-  // baseURL: "http://http://127.0.0.1:8000/api/",
+  // baseURL: "http://127.0.0.1:8000/api/",
   baseURL: "https://thesis.ashkantaravati.ir/api/",
   headers: {
     "Content-type": "application/json",
   },
 });
-
