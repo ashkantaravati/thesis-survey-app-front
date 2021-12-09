053013e4f8e6f97bcbea09eb95c04517609146ba
ashkantaravati <ashkan.taravati@gmail.com>
2021-12-10 01:33:13 +0330

    fix: wrong reference to state [no ci]

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,6 +1,6 @@
 import { ComponentCustomProperties } from "vue";
 import { Store } from "vuex";
-import { State } from "./state";
+import { State } from "@/store/state";
 
 declare module "@vue/runtime-core" {
   interface ComponentCustomProperties {
