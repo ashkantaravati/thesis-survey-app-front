61a3d9d7d05ce9d8335df77bbb2526cc12789deb
ashkantaravati <ashkan.taravati@gmail.com>
2021-12-21 01:57:01 +0330

    setup and use scss

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import { createApp } from "vue";
 import { VueClipboard } from "@soerenmartius/vue3-clipboard";
 import App from "./App.vue";
-import 'element-plus/dist/index.css'
+import "element-plus/dist/index.css";
 import installElementPlus from "./plugins/element";
-import {Delete, Finished, Minus} from "@element-plus/icons"
+import { Delete, Finished, Minus } from "@element-plus/icons";
 import router from "./router";
 import store from "./store";
 import ProceedButton from "@/core/components/ProceedButton.vue";
 import HintDialog from "@/core/components/HintDialog.vue";
-import '@/assets/css/main.css'
+import "@/assets/scss/_app.scss";
 
 const app = createApp(App);
 installElementPlus(app);
 app.use(VueClipboard);
 app.use(router);
 app.use(store);
-app.component("delete-icon",Delete)
-app.component("finished-icon",Finished)
-app.component("minus-icon",Minus)
+app.component("delete-icon", Delete);
+app.component("finished-icon", Finished);
+app.component("minus-icon", Minus);
 app.component("proceed-button", ProceedButton);
 app.component("hint-dialog", HintDialog);
 app.mount("#app");
