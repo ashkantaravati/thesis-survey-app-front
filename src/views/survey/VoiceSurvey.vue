164ad4fbb724789a5212d30fb8613b2c73ca9bf5
ashkantaravati <ashkan.taravati@gmail.com>
2022-01-02 01:07:04 +0330

    change old text

--- src/views/survey/VoiceSurvey.vue
+++ src/views/survey/VoiceSurvey.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="step-container" id="survey-step-5">
     <!-- <h3>ارزیابی رفتار صدای تیم</h3> -->
-    <p>
-      در این قسمت لطفا به ازای هر یک از هم‌تیمی‌های خود به این ۶ سوال پاسخ دهید:
-    </p>
+    <span> سوالات زیر را در رابطه با خودتان و تیم‌تان پاسخ دهید </span>
     <div class="mb-halfrem inner-container">
       <template v-for="(question, index) in voiceSurvey" :key="index">
         <likert-scale-question
